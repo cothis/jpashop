7cbedd0377104bcb34375b3baf54ddab48333a7a
윤민호 <cothis@yunminhoui-MacBookPro.local>
2020-12-19 23:27:57 +0900

    commited

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    protected Address() throws Exception { // jpa 스펙상 추가한 생성자
+        throw new Exception("기본생성자 호출됨");
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
